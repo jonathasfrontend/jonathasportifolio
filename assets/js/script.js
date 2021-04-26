207e6e6050d6dfbfe9fef24b1bce09ca99a8e952
Jonathas <jonathass5678@gmail.com>
2021-04-26 16:19:11 -0300

    Jonathas Portifolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,27 @@ window.addEventListener('scroll', function() {
 AOS.init({
     easing: 'ease-in-out-sine'
   });
+
+// carousel
+
+ $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+          0:{
+            items:1,
+            nav: false
+          },
+          600:{
+            items:2,
+            nav: false
+          },
+          1000:{
+            items:3,
+            nav: false
+          }
+        }
+      });
