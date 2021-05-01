7ca80cf25418ecd54c3e2e2c1a879212b47fc450
Jonathas <jonathass5678@gmail.com>
2021-05-01 16:54:14 -0300

    Jonathas Portifolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,4 +54,10 @@ AOS.init({
             $('.top').removeClass("sticky");
         }
     });
+
+    // Global site tag (gtag.js) - Google Analytics 
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
     
+      gtag('config', 'G-EN3WEQ287C');
